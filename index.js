6d466b4c11dee98b582b5887cd32d409df1f3a25
Garik <someone19911991@gmail.com>
2022-11-02 18:00:51 +0400

    First commit

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const {port } = process.env || 5000;
 const {frontUrl} = process.env
 app.use(express.json());
 app.use(cors({ origin: "*" }));
-//
 app.use('/api', routing);
-// app.get('/', (req, res) => res.send('Hello World man!'));
-app.get('/', (req, res) => {
-    res.send('Hi everyone');
-});
+
 
 app.use(errorMiddleware);
 
