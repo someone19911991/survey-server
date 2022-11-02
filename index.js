954688b4ad394d9265bac01f9eecec6c9e916cef
Garik <someone19911991@gmail.com>
2022-11-02 16:53:38 +0400

    trial

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(errorMiddleware);
 // const app = express();
 // const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World man!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
