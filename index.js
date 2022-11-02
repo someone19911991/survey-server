f6ef884f16449e98225bce1311848b8ac32b6a50
Garik <someone19911991@gmail.com>
2022-11-02 16:55:45 +0400

    trial

--- index.js
+++ index.js
@@ -10,18 +10,12 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 //
 app.use('/api', routing);
-// app.get('/', (req, res) => {
-//     res.send('Hi everyone');
-// });
-//
+// app.get('/', (req, res) => res.send('Hello World man!'));
+app.get('/', (req, res) => {
+    res.send('Hi everyone');
+});
+
 app.use(errorMiddleware);
-//
-// app.listen(port, () => console.log(`App running on port ${port}`));
 
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World man!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
