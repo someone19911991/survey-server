43f2c486021817ff6bd7ccc569910877bf104595
Garik <someone19911991@gmail.com>
2022-11-02 16:49:32 +0400

    trial

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 // const routing = require('./routing/index');
 // const errorMiddleware = require("./middlewares/error.middleware");
 const {port } = process.env || 5000;
 const {frontUrl} = process.env
-// app.use(express.json());
-// app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 //
 // app.use('/api', routing);
 // app.get('/', (req, res) => {
@@ -18,8 +18,8 @@ const {frontUrl} = process.env
 //
 // app.listen(port, () => console.log(`App running on port ${port}`));
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 // const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
