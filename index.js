b87267922f2bdca14956262e633a771ddd7b9c30
Garik <someone19911991@gmail.com>
2022-11-02 16:37:07 +0400

    trial

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const routing = require('./routing/index');
+const errorMiddleware = require("./middlewares/error.middleware");
+const {port, frontUrl} = process.env || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.use('/api', routing);
+app.get('/', (req, res) => {
+    res.send('Hi everyone');
+});
 
-// require('dotenv').config();
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// const routing = require('./routing/index');
-// const errorMiddleware = require("./middlewares/error.middleware");
-// const {port, frontUrl} = process.env || 5000;
-//
-// app.use(express.json());
-// app.use(cors({ origin: "*" }));
-//
-// app.use('/api', routing);
-// app.get('/', (req, res) => {
-//     res.send('Hi everyone');
-// });
-//
-// app.use(errorMiddleware);
-//
-// app.listen(port, () => console.log(`App running on port ${port}`));
+app.use(errorMiddleware);
+
+app.listen(port, () => console.log(`App running on port ${port}`));
